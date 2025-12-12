d1706c72e570a999760e7d0e399b4f35d919982d
Atul kumar singh <atulkumarsingh958@gmail.com>
2025-12-12 20:59:56 +0530

    add data

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,89 +1,4 @@
-
-
-
-// import React, { useState } from "react";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-
-// function Create() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const navigate = useNavigate();
-
-//   const headers = { "Access-Control-Allow-Origin": "*" };
-
-//   const handleCreate = async (e) => {
-//     e.preventDefault();
-//     try {
-//       await axios.post(
-//         "https://68c489ab81ff90c8e61c80df.mockapi.io/fakeData",
-//         { name, email },
-//         { headers }
-//       );
-
-//       alert("Product created successfully ");
-//       setName("");
-//       setEmail("");
-//       navigate("/read"); // redirect to Read page
-//     } catch (error) {
-//       console.error("Error creating product:", error);
-//       alert("Something went wrong ");
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1 style={{ color: "red" }}>CREATE</h1>
-
-//       <div>
-//         <h4>Name</h4>
-//         <input
-//           type="text"
-//           placeholder="Enter name"
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//       </div>
-
-//       <br />
-
-//       <div>
-//         <h4>Email</h4>
-//         <input
-//           type="email"
-//           placeholder="Enter email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//       </div>
-
-//       <br />
-
-//       <button onClick={handleCreate}>Add</button>
-//     </div>
-//   );
-// }
-
-// export default Create;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
@@ -104,8 +19,6 @@ const handleCreate = async(e) =>{
     setEmail('');
 
     navigate('/read')
-
-
 
   }
   catch (err){
@@ -137,13 +50,6 @@ value={email}
 onChange={(e) => setEmail(e.target.value)}
 />
  </div>
-
-
-<br />
-
-
-
-
 <button type='submit'> Add  </button>
 
         </form>
@@ -154,3 +60,14 @@ onChange={(e) => setEmail(e.target.value)}
 }
 
 export default Create
+
+
+
+
+
+
+
+
+
+
+
--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,104 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import { Link, useNavigate } from "react-router-dom";
-
-// function Read() {
-//   const [data, setData] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const navigate = useNavigate();
-
-  
-//   useEffect(() => {
-//     setLoading(true);
-//     axios
-//       .get("https://68c489ab81ff90c8e61c80df.mockapi.io/fakeData")
-//       .then((res) => {
-//         setData(res.data);
-//         setLoading(false);
-//       })
-//       .catch((err) => {
-//         console.error("Fetch error:", err);
-//         setError("Something went wrong while fetching data!");
-//         setLoading(false);
-//       });
-//   }, []);
-
-  
-//   if (loading) {
-//     return <h3 style={{ color: "blue" }}>Loading data...</h3>;
-//   }
-
-//   if (error) {
-//     return <h3 style={{ color: "red" }}>{error}</h3>;
-//   }
-
-//   return (
-//     <div>
-//       <h1 style={{ color: "green" }}>Read Data</h1>
-
-//       {data.length === 0 ? (
-//         <p>No records found. Please add some data.</p>
-//       ) : (
-//         <table
-//           border="1"
-//           cellPadding="10"
-//           style={{ borderCollapse: "collapse", width: "100%" }}
-//         >
-//           <thead>
-//             <tr style={{ backgroundColor: "#f2f2f2" }}>
-//               <th>ID</th>
-//               <th>Name</th>
-//               <th>Email</th>
-//               <th>Actions</th>
-//             </tr>
-//           </thead>
-
-//           <tbody>
-//             {data.map((item) => (
-//               <tr key={item.id}>
-//                 <td>{item.id}</td>
-//                 <td>{item.name}</td>
-//                 <td>{item.email}</td>
-//                 <td>
-//                   {/* Edit navigates to Update.jsx with ID */}
-//                   <button
-//                     style={{ marginRight: "10px" }}
-//                     onClick={() => navigate(`/update/${item.id}`)}
-//                   >
-//                      Edit
-//                   </button>
-
-//                   {/* Delete navigates to Delete.jsx with ID */}
-//                   <Link to={`/delete/${item.id}`}>
-//                     <button style={{ color: "white", background: "red" }}>
-//                       Delete
-//                     </button>
-//                   </Link>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       )}
-
-//       <br />
-//       <Link to="/" style={{ color: "blue", textDecoration: "underline" }}>
-//          Add New
-//       </Link>
-//     </div>
-//   );
-// }
-
-// export default Read;
-
-
-
-
-
-
-
-
 
 
 
@@ -115,10 +14,7 @@ function Read() {
     axios.get('https://68c489ab81ff90c8e61c80df.mockapi.io/fakeData')
     .then((res) =>{
       setData(res.data);
-      
-      
-
-
+    
     })
     .catch((err) => console.error('something went wrong' , err))
     
